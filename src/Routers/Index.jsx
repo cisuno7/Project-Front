9ce761d8a80c32e3662899d60bc0df774a6eb857
cisuno7 <cisuno7@hotmail.com>
2023-08-25 15:13:13 -0300

    front estruturado

--- src/Routers/Index.jsx
+++ src/Routers/Index.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FormularioPage from '../Screens/Formulario/Index';
 import LoginPage from '../Screens/Login/Index';
 import HomePage from '../Screens/Home/Index';
-
+import TabelaPage from '../Screens/Tabela/Index'
 function App() {
   return (
     <div className="app-container">
@@ -12,6 +12,7 @@ function App() {
           <Route path="/" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/formulario" element={<FormularioPage />} />
+          <Route path="/Tabela" element={<TabelaPage />} />
         </Routes>
       </Router>
     </div>
